25f54528a88fe3b7572df30aaae61e7a0b89d396
Andrew Thorp <andrew.thorp.dev@gmail.com>
2019-10-22 10:21:11 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='bb84',    # This is the name of your PyPI-package.
     version='1.0',                          # Update the version number for new releases
-    scripts=[]                  # The name of your scipt, and also the command you'll be using for calling it
+    packages=find_packages(),
+    scripts=['alice.py', 'bob.py'],
+    install_requires=['bitvector', 'pycrypto', 'cqc'],
+    author='Andrew Thorp',
+    author_email='andrew.thorp.dev@gmail.com',
+    url='https://github.com/athorp96/bb84',
 )
 
