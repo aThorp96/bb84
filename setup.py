9382a7217dd2ca9d3e3dabb9b7cdf23f70365771
Andrew Thorp <andrew.thorp.dev@gmail.com>
2019-10-22 09:03:59 -0400

    Add simple setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='bb84',    # This is the name of your PyPI-package.
+    version='1.0',                          # Update the version number for new releases
+    scripts=[]                  # The name of your scipt, and also the command you'll be using for calling it
+)
+
